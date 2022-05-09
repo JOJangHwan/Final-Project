0defec852f6d37692679cdc11ddde19eb3dba579
임현성 <zaqxsw4564@naver.com>
2022-05-10 00:34:23 +0900

    지도, 사용자 현위치

--- app/src/main/java/com/example/virus_confirmation_management_application/user_nearhosfind.java
+++ app/src/main/java/com/example/virus_confirmation_management_application/user_nearhosfind.java
@@ -1,13 +1,67 @@
 package com.example.virus_confirmation_management_application;
 
+import android.Manifest;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapView;
+import com.skt.Tmap.TMapGpsManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class user_nearhosfind extends AppCompatActivity {
+public class user_nearhosfind extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
 
+    String API_Key = "l7xxf29dc0cd31554f918e23f8d747e9ca1d";
+
+    // T Map View
+    TMapView tMapView = null;
+
+    // T Map GPS
+    TMapGpsManager tMapGPS = null;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_nearhosfind_activity);
+
+        // T Map View
+        tMapView = new TMapView(this);
+
+        // API Key
+        tMapView.setSKTMapApiKey(API_Key);
+
+        // Initial Setting
+        tMapView.setZoomLevel(17);
+        tMapView.setIconVisibility(true);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        // T Map View Using Linear Layout
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        linearLayoutTmap.addView(tMapView);
+
+        // Request For GPS permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        // GPS using T Map
+        tMapGPS = new TMapGpsManager(this);
+
+        // Initial Setting
+        tMapGPS.setMinTime(1000);
+        tMapGPS.setMinDistance(10);
+        tMapGPS.setProvider(tMapGPS.NETWORK_PROVIDER);
+        //tMapGPS.setProvider(tMapGPS.GPS_PROVIDER);
+
+        tMapGPS.OpenGps();
+    }
+
+    @Override
+    public void onLocationChange(Location location) {
+        tMapView.setLocationPoint(location.getLongitude(), location.getLatitude());
+        tMapView.setCenterPoint(location.getLongitude(), location.getLatitude());
     }
 }
